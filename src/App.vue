ccb4ffed0865556e05cefd631d50f0d11abdbb75
Herbert <herbertlzx@gmail.com>
2023-08-08 08:54:04 +0800

    feat: darkmode

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <div >
+  <div>
     <div class="area-input">
       <!-- <h2>为什么人有这么多为什么</h2> -->
-      <el-form @submit.prevent @keyup.enter.native="onSubmit" :model="formdata" label-width="120px">
-        <el-input v-model="formdata.question" placeholder="请输入问题" autofocus="autofocus" clearable>
+      <el-form
+        @submit.prevent
+        @keyup.enter.native="onSubmit"
+        :model="formdata"
+        label-width="120px"
+      >
+        <el-input
+          v-model="formdata.question"
+          placeholder="请输入问题"
+          autofocus="autofocus"
+          clearable
+        >
           <template #append>
-            <el-button :icon="Search" :loading="loading" :disabled="loading" @click="onSubmit" />
+            <el-button
+              :icon="Search"
+              :loading="loading"
+              :disabled="loading"
+              @click="onSubmit"
+            />
           </template>
         </el-input>
       </el-form>
@@ -13,45 +28,52 @@
     <div class="area-result" v-loading="loading">
       <pre v-html="formdata.result"></pre>
     </div>
-
   </div>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue'
-import { Search } from '@element-plus/icons-vue'
-const loading = ref(false)
+import { ref, reactive, onMounted } from 'vue';
+import { Search } from '@element-plus/icons-vue';
+import { auto as followSystemColorScheme } from 'darkreader';
+const loading = ref(false);
 const formdata = reactive({
   question: '',
-  result: ''
-})
+  result: '',
+});
 const onSubmit = () => {
-  console.log(formdata.question)
+  console.log(formdata.question);
   getResult();
-}
+};
 // 回调函数
 const getResult = () => {
-  loading.value = true
+  loading.value = true;
   fetch('https://glm.aiursoft.cn', {
-    method: "post",
+    method: 'post',
     body: JSON.stringify({
-      prompt: formdata.question
+      prompt: formdata.question,
+    }),
+  })
+    .then((res) => {
+      return res.json();
     })
-  }).then(res => {
-    return res.json()
-  }).then(data => {
-    loading.value = false
-    formdata.result = data.response
-  }).catch(function (error) {
-    loading.value = false
-    formdata.result = error.message
-    console.log('错误信息：' + error.message);
-  });
-}
+    .then((data) => {
+      loading.value = false;
+      formdata.result = data.response;
+    })
+    .catch(function (error) {
+      loading.value = false;
+      formdata.result = error.message;
+      console.log('错误信息：' + error.message);
+    });
+};
+onMounted(() => {
+  followSystemColorScheme();
+});
 </script>
 
 <style lang="less">
-html,body{
+html,
+body {
   margin: 0;
   height: 100%;
   overflow: hidden;
@@ -72,7 +94,6 @@ html,body{
   }
 }
 
-
 .area-input {
 }
 
@@ -87,5 +108,4 @@ pre {
   padding: 20px;
   border-radius: 10px;
 }
-
 </style>
