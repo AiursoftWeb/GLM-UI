3a840b4bc86d05bc9435a854e4922be0788d1845
linpengchao <herbertlzx@gmail.com>
2023-08-08 21:41:24 +0800

    feat: history&reset

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,13 @@
   </div>
   <div class="answer-container">
     <div class="area-input">
+      <el-button @click="reset" size="large">清空</el-button>
       <el-form
         @submit.prevent
         @keyup.enter.native="onSubmit"
         :model="formdata"
         label-width="120px"
+        style="width: 100%"
       >
         <el-input
           v-model="formdata.question"
@@ -24,6 +26,7 @@
           placeholder="请输入问题"
           autofocus="autofocus"
           clearable
+          size="large"
         >
           <template #append>
             <el-button
@@ -40,14 +43,13 @@
 </template>
 
 <script setup>
-import { ref, reactive, onMounted, nextTick } from 'vue';
-import { Search } from '@element-plus/icons-vue';
-import { auto as followSystemColorScheme } from 'darkreader';
+import { ref, reactive, onMounted, nextTick } from "vue";
+import { Search } from "@element-plus/icons-vue";
+import { auto as followSystemColorScheme } from "darkreader";
 const loading = ref(false);
 const scrollContainer = ref(null);
 const formdata = reactive({
-  question: '',
-  result: '',
+  question: "",
 });
 const dialogue = reactive({
   messages: [],
@@ -55,29 +57,42 @@ const dialogue = reactive({
 const onSubmit = () => {
   getResult();
 };
+const reset = () => {
+  formdata.question = "";
+  dialogue.messages = [];
+};
 const getResult = async () => {
   loading.value = true;
+  const result = dialogue.messages.reduce((acc, curr, idx) => {
+    if (idx % 2 === 0) {
+      //当索引值是偶数时，创建一个新的子数组
+      acc.push([curr.content]);
+    } else {
+      //当索引值是奇数时，将当前元素加到上一个子数组
+      acc[acc.length - 1].push(curr.content);
+    }
+    return acc;
+  }, []);
+  await nextTick();
   dialogue.messages.push({ content: formdata.question, isUser: true });
-  dialogue.messages.push({ content: '', isUser: false, loading: true });
   scrollToBottom();
-  const ans = await fetch('https://glm.aiursoft.cn', {
-    method: 'post',
+  const ans = await fetch("https://glm.aiursoft.cn", {
+    method: "post",
     body: JSON.stringify({
       prompt: formdata.question,
+      history: result,
     }),
   }).catch(function (error) {
     loading.value = false;
-    dialogue.messages.pop();
     dialogue.messages.push({ content: error, isUser: false });
-    formdata.question = '';
+    formdata.question = "";
     scrollToBottom();
   });
   const data = await ans.json();
   console.log(data);
   loading.value = false;
-  dialogue.messages.pop();
   dialogue.messages.push({ content: data.response, isUser: false });
-  formdata.question = '';
+  formdata.question = "";
   scrollToBottom();
 };
 const scrollToBottom = async () => {
@@ -85,7 +100,7 @@ const scrollToBottom = async () => {
   if (scrollContainer.value) {
     scrollContainer.value.scrollTo({
       top: scrollContainer.value.scrollHeight,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   }
 };
@@ -101,6 +116,7 @@ body {
   height: 100%;
   overflow: hidden;
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -110,6 +126,7 @@ body {
   padding: 4rem 20vw;
   height: 100%;
   overflow-y: auto;
+
   @media screen and (max-width: 768px) {
     &#app {
       padding: 2rem 5vw;
@@ -118,6 +135,8 @@ body {
 }
 
 .area-input {
+  display: flex;
+  gap: 10px;
   margin-top: 20px;
   width: 100%;
 }
@@ -160,10 +179,12 @@ body {
   border: 1px solid #ccc;
   border-radius: 5px;
 }
+
 .answer-container {
   width: 600px;
   margin: 0 auto;
 }
+
 @media screen and (max-width: 768px) {
   .area-input {
     width: 100%;
@@ -177,10 +198,12 @@ body {
   .dialog {
     padding: 10px;
   }
+
   .answer-container {
     width: 90%;
     margin: 0 auto;
   }
+
   .message {
     max-width: 80%;
   }
